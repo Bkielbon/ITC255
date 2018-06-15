8c182ebf694d79e493c239da698040ceec1e873c
Bennett <35482813+Bkielbon@users.noreply.github.com>
2018-06-15 15:55:15 -0700

    Create Student.java

---
+++ Class Coding Individual/Student.java
@@ -0,0 +1,38 @@
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Student extends AbstractPerson{
+	private ArrayList<Certification> certification;
+	private double amt;
+
+	public Student(String fn, String ln, String e, String pn, String lp) {
+		super(ln, e, pn, lp, lp);
+		certification = new ArrayList<Certification>();
+	}
+
+	public void makeDonation(double amt, Date donationDate){
+		Certification certification = new Certification(amt, this, donationDate);
+		certification.add(certification);
+	}
+	
+	public String printStudent(){
+		return "This Student's info: \n"
+				+ "First Name: \t" + this.getFirstName() + "\n"
+				+ "Last Name: \t" + this.getLastName() + "\n"
+				+ "E-Mail: \t" + this.getEmail() + "\n"
+				+ "Phone Number: \t" + this.getPhoneNumber() + "\n"
+				+ "Login Password: \t" + this.getLoginPassword() + "\n" + 
+				"\n" +
+				certification;
+		
+	}
+	
+	public static void Login(String userName, String password)
+	{
+	   System.out.println("Login Successfull");
+	 
+	}
+
+}
